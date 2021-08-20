9eaa6d45a6078908c07be6ce7cb00510720f018f
Michael Katsov <michakatsov@gmail.com>
2021-08-21 00:40:30 +0300

    -changed code

--- bot.js
+++ bot.js
@@ -1,4 +1,11 @@
-// require the needed discord.js classes
 const { Client, Intents } = require('discord.js');
+const fs = require('fs')
 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+let token = fs.readFileSync("../token.txt", "utf-8");
+client.login(token);
