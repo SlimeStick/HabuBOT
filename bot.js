931a6751062238549306be351b9309dcfd8b4d30
Michael Katsov <michakatsov@gmail.com>
2021-08-20 23:11:35 +0300

    -added file: bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('your-token-goes-here');
