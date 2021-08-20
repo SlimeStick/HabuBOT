18e727aa83f764db9176941514216a7e1b0b133a
Michael Katsov <michakatsov@gmail.com>
2021-08-21 00:05:10 +0300

    -deleted project, cause why not

--- bot.js
+++ bot.js
@@ -1,19 +1,1 @@
-// require the needed discord.js classes
-const { Client, Intents } = require('discord.js');
-const { fs } = require('fs');
-console.log("hello");
-// create a new Discord client
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-// login to Discord with your app's token
-fs.readFile("./../token.txt", function(err, token){
-    console.log(token);
-	client.login(token);
-});
 
