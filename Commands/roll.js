ab1d8bb090c408b66b916b1a62e320343638c0e5
David Khitrik <david.khitrik@gmail.com>
2021-09-14 20:42:12 +0300

    roll

---
+++ Commands/roll.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "roll",
+    description: "rolls a number between 1-6",
+    execute(message,args){
+        message.reply('kziza 6')
+    }
+}
--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
-const { token } = require('./config.json');
+const {token, prefix} = require('./config.json');
+const fs = require('fs');
 
 const client = new Discord.Client();
 
@@ -7,8 +8,48 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
-	console.log("message detected");
+client.commands = new Discord.Collection();
+
+//finding all of the commands
+const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
+
+//adding commands to the bot
+for(const file of commandFiles)
+{
+    const command = require(`./Commands/${file}`);
+	
+    client.commands.set(command.name, command);
+}
+
+client.on('message', (msg) =>{
+    //checking if the bot is in a guild(discord server) and if the message wasn't sent by a bot
+    let guildName = msg.guild.name;
+    if(!msg.author.bot && guildName != null)
+    {
+        //checks if the message is a command
+        if(msg.content.startsWith(prefix))
+        {
+            //saves the arguments and the command itself in a variable
+            let args = msg.content.slice(prefix.length).trim().split(' ');
+            let command = args.shift().toLowerCase();
+            if(client.commands.has(command))
+            {
+                try
+                {
+                    client.commands.get(command).command(msg, args, client);
+                }
+                catch(error)
+                {
+                    console.error(error);
+                    msg.reply(error);
+                }
+            }
+            else
+            {
+                msg.reply("this command doesn't exist");
+            }
+        }
+    }
 });
 
 client.login(token);
