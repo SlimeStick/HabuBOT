d11c3c6a81e6d26690e59e6b5e58e547d67ddb6b
Michael Katsov <michakatsov@gmail.com>
2021-09-14 19:07:08 +0300

    changes

--- bot.js
+++ bot.js
@@ -1,10 +1,19 @@
 const { Client, Intents } = require('discord.js');
 const fs = require('fs');
-const { token, prefix } = require('config.json');
+const { token, prefix } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+client.on('message', async message => {
+	console.log("interaction");
+	if (!message.content.startsWith(prefix)) return;
+
+	const { commandName } = interaction;
+	console.log(commandName);
 });
 
 client.login(token);
