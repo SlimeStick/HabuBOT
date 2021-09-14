337f617447046b26db4039fee0c2f95893ae2146
Michael Katsov <michakatsov@gmail.com>
2021-09-14 18:51:49 +0300

    changed bot js

--- bot.js
+++ bot.js
@@ -1,12 +1,10 @@
 const { Client, Intents } = require('discord.js');
-const fs = require('fs')
-
-const prefix = ";"
+const fs = require('fs');
+const { token, prefix } = require('config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-let token = fs.readFileSync("../token.txt", "utf-8");
 client.login(token);
