d4061e5cc4942813d55d73c349e7aea86eb7be07
Michael Katsov <michakatsov@gmail.com>
2021-09-14 20:12:41 +0300

    fuck the new version

--- bot.js
+++ bot.js
@@ -1,19 +1,14 @@
-const { Client, Intents } = require('discord.js');
-const fs = require('fs');
-const { token, prefix } = require('./config.json');
+const Discord = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', async message => {
-	console.log("interaction");
-	if (!message.content.startsWith(prefix)) return;
-
-	const { commandName } = interaction;
-	console.log(commandName);
+client.on('message', message => {
+	console.log("message detected");
 });
 
 client.login(token);
